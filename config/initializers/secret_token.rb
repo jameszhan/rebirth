6b7ff517295d37de74eee60431487b299f77116b
James Zhan <zhiqiangzhan@gmail.com>
2012-11-26 18:39:13 +0800

    rails g mongoid:config

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rebirth::Application.config.secret_token = 'fa2a0bbc7d41cab0db2a8c6b480a192527d97524793290bd8c26d343c69c7b6da98c8b3a610d22487ea8863fdf10e4f0d2c52e1452ba0180e608409596236950'
