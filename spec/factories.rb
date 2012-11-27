d7b85792bf801a1d24a750694af5b5ad26239e6e
James Zhan <zhiqiangzhan@gmail.com>
2012-11-27 16:12:32 +0800

    setup the devise.

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,13 +1,3 @@
 FactoryGirl.define do
-  factory :user do
-    sequence(:name) {|n|"user-#{n}"}
-    email           {|u|"#{u.name}@example.com"}
-    password        "secret"
-    admin           false
 
-    factory :admin do
-      admin true
-    end
-
-  end
 end
